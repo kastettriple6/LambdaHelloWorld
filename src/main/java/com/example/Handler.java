758d46bd4e37f2b873a98b28f5cdbe1b8090515d
kastettriple6 <65229792+kastettriple6@users.noreply.github.com>
2021-08-18 14:41:25 +0300

    install maven

--- src/main/java/com/example/Handler.java
+++ src/main/java/com/example/Handler.java
@@ -2,12 +2,15 @@ package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
 
 import java.util.Map;
 
 public class Handler implements RequestHandler<Map<String,Object>, String> {
     @Override
     public String handleRequest(Map<String, Object> stringObjectMap, Context context) {
-        return "Hello World AWS";
+        Gson gson = new Gson();
+        String message = "Hello World";
+        return gson.toJson(message);
     }
 }
