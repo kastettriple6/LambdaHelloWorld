4761ab8939aa8b439768845bd6b3df8398f78822
kastettriple6 <65229792+kastettriple6@users.noreply.github.com>
2021-08-19 20:35:24 +0300

    airtable

--- src/main/java/com/example/service/AirTableService.java
+++ src/main/java/com/example/service/AirTableService.java
@@ -1,15 +1,12 @@
-package com.example;
+package com.example.service;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import dev.fuxing.airtable.AirtableApi;
 import dev.fuxing.airtable.AirtableRecord;
 import dev.fuxing.airtable.AirtableTable;
 
-import java.util.Map;
 import java.util.Properties;
 
-public class Handler implements RequestHandler<Map<String, Object>, AirtableRecord> {
+public class AirTableService {
     private final Properties props = new Properties();
     private final String KEY = props.getProperty("airtable.api.key");
     private final String APP = props.getProperty("airtable.api.app");
@@ -17,8 +14,13 @@ public class Handler implements RequestHandler<Map<String, Object>, AirtableReco
     private final AirtableApi API = new AirtableApi(KEY);
     private final AirtableTable TABLE = API.base(APP).table("MainTable.csv");
 
-    @Override
-    public AirtableRecord handleRequest(Map<String, Object> stringObjectMap, Context context) {
+    public AirtableRecord getSingleRecord() {
         return TABLE.get(FIRST_RECORD);
+    }
+
+    public AirtableTable.PaginationList getAllRecords() {
+        return TABLE.list(querySpec -> {
+            querySpec.fields("title");
+        });
     }
 }
