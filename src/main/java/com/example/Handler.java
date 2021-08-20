bb23977f36e092e64d25cab414fcdd59b41c95ce
kastettriple6 <65229792+kastettriple6@users.noreply.github.com>
2021-08-20 22:06:43 +0300

    update cfg

--- src/main/java/com/example/Handler.java
+++ src/main/java/com/example/Handler.java
@@ -13,12 +13,13 @@ public class Handler implements RequestHandler<Map<String, Object>, AirtableReco
     private final Properties props = new Properties();
     private final String KEY = props.getProperty("airtable.api.key");
     private final String APP = props.getProperty("airtable.api.app");
+    private final String TABLE = props.getProperty("airtable.api.table");
 
     @Override
     public AirtableRecord handleRequest(Map<String, Object> stringObjectMap, Context context) {
         AirtableApi api = new AirtableApi(KEY);
         AirtableApi.Application base = api.base(APP);
-        AirtableTable table = base.table("MainTable");
+        AirtableTable table = base.table(TABLE);
 
         while (true) {
             int i = 0;
@@ -46,5 +47,4 @@ public class Handler implements RequestHandler<Map<String, Object>, AirtableReco
             }
         }
     }
-
 }
