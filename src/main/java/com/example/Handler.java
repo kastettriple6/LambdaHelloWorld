fdbd92ccb6775f83acfd1ee8f99320a4f293410b
kastettriple6 <65229792+kastettriple6@users.noreply.github.com>
2021-08-20 22:19:58 +0300

    update

--- src/main/java/com/example/Handler.java
+++ src/main/java/com/example/Handler.java
@@ -2,21 +2,21 @@ package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
 import dev.fuxing.airtable.AirtableApi;
-import dev.fuxing.airtable.AirtableRecord;
 import dev.fuxing.airtable.AirtableTable;
 
 import java.util.Map;
 import java.util.Properties;
 
-public class Handler implements RequestHandler<Map<String, Object>, AirtableRecord> {
+public class Handler implements RequestHandler<Map<String, Object>, JsonNode> {
     private final Properties props = new Properties();
     private final String KEY = props.getProperty("airtable.api.key");
     private final String APP = props.getProperty("airtable.api.app");
     private final String TABLE = props.getProperty("airtable.api.table");
 
     @Override
-    public AirtableRecord handleRequest(Map<String, Object> stringObjectMap, Context context) {
+    public JsonNode handleRequest(Map<String, Object> stringObjectMap, Context context) {
         AirtableApi api = new AirtableApi(KEY);
         AirtableApi.Application base = api.base(APP);
         AirtableTable table = base.table(TABLE);
