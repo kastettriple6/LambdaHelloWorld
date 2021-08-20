9e508262068db1ee94bf2ff70e06a77f111363ad
kastettriple6 <65229792+kastettriple6@users.noreply.github.com>
2021-08-21 00:20:36 +0300

    update

--- src/main/java/com/example/Handler.java
+++ src/main/java/com/example/Handler.java
@@ -2,38 +2,26 @@ package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dev.fuxing.airtable.AirtableApi;
 import dev.fuxing.airtable.AirtableTable;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-public class Handler implements RequestHandler<String, String> {
+public class Handler implements RequestHandler<String, JsonNode> {
 
     @Override
-    public String handleRequest(String s, Context context) {
-        Properties props = new Properties();
-        InputStream is = getClass().getClassLoader().getResourceAsStream("config.properties");
-        try {
-            props.load(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public JsonNode handleRequest(String s, Context context) {
+        AirtableApi api = new AirtableApi("keylMkRVCV25NxOBl");
+        AirtableApi.Application base = api.base("appW1Vv1p4IHxtiWQ");
+        AirtableTable table = base.table("tblX1jGmYZoqyHrlE");
 
-        String key = props.getProperty("airtable.api.key");
-        String app = props.getProperty("airtable.api.app");
-        String tableId = props.getProperty("airtable.api.table");
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
 
-        try {
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        AirtableApi api = new AirtableApi(key);
-        AirtableApi.Application base = api.base(app);
-        AirtableTable table = base.table(tableId);
+        ObjectMapper objectMapper = new ObjectMapper();
 
         while (true) {
             int i = 0;
@@ -43,11 +31,16 @@ public class Handler implements RequestHandler<String, String> {
                     if (j == table.list().size()) {
                         j = 0;
                     }
-                    System.out.println(table.list(querySpec -> {
-                        querySpec.sort("ID");
-                        querySpec.fields("title");
-                    })
-                            .get(j).getField("title"));
+                    try {
+                        String json = objectMapper.writeValueAsString(
+                        table.list(querySpec -> {
+                            querySpec.sort("ID");
+                            querySpec.fields("title");
+                        })
+                                .get(j).getField("title"));
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
                     j++;
                 }
                 try {
