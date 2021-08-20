a2a724918c0f77c582eb00d87005cf59f00c9e89
kastettriple6 <65229792+kastettriple6@users.noreply.github.com>
2021-08-20 21:52:14 +0300

    airtable complete

--- src/main/java/com/example/Handler.java
+++ src/main/java/com/example/Handler.java
@@ -13,12 +13,38 @@ public class Handler implements RequestHandler<Map<String, Object>, AirtableReco
     private final Properties props = new Properties();
     private final String KEY = props.getProperty("airtable.api.key");
     private final String APP = props.getProperty("airtable.api.app");
-    private final String FIRST_RECORD = props.getProperty("airtable.record.index.0");
-    private final AirtableApi API = new AirtableApi(KEY);
-    private final AirtableTable TABLE = API.base(APP).table("MainTable");
 
     @Override
     public AirtableRecord handleRequest(Map<String, Object> stringObjectMap, Context context) {
-        return TABLE.get(FIRST_RECORD);
+        AirtableApi api = new AirtableApi(KEY);
+        AirtableApi.Application base = api.base(APP);
+        AirtableTable table = base.table("MainTable");
+
+        while (true) {
+            int i = 0;
+            while (i < table.list().size()) {
+                int j = i;
+
+                for (int k = 0; k < 3; k++) {
+                    if (j == table.list().size()) {
+                        j = 0;
+                    }
+                    System.out.println(table.list(querySpec -> {
+                        querySpec.sort("ID");
+                        querySpec.fields("title");
+                    })
+                            .get(j).getField("title"));
+                    j++;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                i++;
+            }
+        }
     }
+
 }
