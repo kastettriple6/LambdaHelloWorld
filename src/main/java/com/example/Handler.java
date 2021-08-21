47273ede9013abe49ac71a87e308bfac0e87ea06
kastettriple6 <65229792+kastettriple6@users.noreply.github.com>
2021-08-22 00:48:57 +0300

    update

--- src/main/java/com/example/Handler.java
+++ src/main/java/com/example/Handler.java
@@ -2,55 +2,67 @@ package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import dev.fuxing.airtable.AirtableApi;
 import dev.fuxing.airtable.AirtableTable;
 
-public class Handler implements RequestHandler<String, JsonNode> {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+public class Handler implements RequestHandler<Map<String, Object>, String> {
 
     @Override
-    public JsonNode handleRequest(String s, Context context) {
-        AirtableApi api = new AirtableApi("keylMkRVCV25NxOBl");
-        AirtableApi.Application base = api.base("appW1Vv1p4IHxtiWQ");
-        AirtableTable table = base.table("tblX1jGmYZoqyHrlE");
+    public String handleRequest(Map<String, Object> s, Context context) {
+        InputStream is;
+        Properties prop = new Properties();
+        is = getClass().getClassLoader().getResourceAsStream("config.properties");
+        try {
+            prop.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
+        String key = prop.getProperty("airtable.api.key");
+        String app = prop.getProperty("airtable.api.app");
+        String tableId = prop.getProperty("airtable.api.table");
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        AirtableApi api = new AirtableApi(key);
+        AirtableApi.Application base = api.base(app);
+        AirtableTable table = base.table(tableId);
 
-        while (true) {
-            int i = 0;
-            while (i < table.list().size()) {
-                int j = i;
-                for (int k = 0; k < 3; k++) {
-                    if (j == table.list().size()) {
-                        j = 0;
-                    }
-                    try {
-                        String json = objectMapper.writeValueAsString(
-                        table.list(querySpec -> {
+        String printing = "";
+        try {
+            while (true) {
+                int i = 0;
+                while (i < table.list().size()) {
+                    int j = i;
+                    for (int k = 0; k < 3; k++) {
+                        if (j == table.list().size()) {
+                            j = 0;
+                        }
+                        JsonNode response = table.list(querySpec -> {
                             querySpec.sort("ID");
                             querySpec.fields("title");
                         })
-                                .get(j).getField("title"));
-                    } catch (JsonProcessingException e) {
+                                .get(j).getField("title");
+                        printing = response.toPrettyString();
+                        System.out.println(printing);
+                        j++;
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    j++;
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
 
-                i++;
+                    i++;
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return printing;
     }
 }
