814a4019dfd8c8f66b4cd99fea416710a225fad5
kastettriple6 <65229792+kastettriple6@users.noreply.github.com>
2021-08-26 13:58:04 +0300

    update

--- src/main/java/com/example/Handler.java
+++ src/main/java/com/example/Handler.java
@@ -8,60 +8,63 @@ import dev.fuxing.airtable.AirtableTable;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Handler implements RequestHandler<Map<String, Object>, String> {
+public class Handler implements RequestHandler<Map<String, Object>, List<String>> {
+    private InputStream is;
+    private Properties prop = new Properties();
+    private Offset offset = new Offset();
 
     @Override
-    public String handleRequest(Map<String, Object> s, Context context) {
-        InputStream is;
-        Properties prop = new Properties();
+    public List<String> handleRequest(Map<String, Object> s, Context context) {
+
+        int offs = offset.get();
+        return Arrays.asList(getRecord(offs), getRecord(offs + 1), getRecord(offs + 2));
+    }
+
+
+    public String getRecord(int index) {
+        String key = prop.getProperty("airtable.api.key");
+        String app = prop.getProperty("airtable.api.app");
+        String tableId = prop.getProperty("airtable.api.table");
+
         is = getClass().getClassLoader().getResourceAsStream("config.properties");
         try {
             prop.load(is);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        String key = prop.getProperty("airtable.api.key");
-        String app = prop.getProperty("airtable.api.app");
-        String tableId = prop.getProperty("airtable.api.table");
-
         AirtableApi api = new AirtableApi(key);
         AirtableApi.Application base = api.base(app);
-        AirtableTable table = base.table(tableId);
+        AirtableApi.Table table = base.table(tableId);
 
-        String printing = "";
+        index %= table.list().size();
+        JsonNode response = table.list(querySpec -> {
+            querySpec.sort("ID");
+            querySpec.fields("title");
+        })
+                .get(index).getField("title");
+        return response.toPrettyString();
+    }
+}
 
-        while (true) {
-            int i = 0;
-            while (i < table.list().size()) {
-                int j = i;
-                for (int k = 0; k < 3; k++) {
-                    if (j == table.list().size()) {
-                        j = 0;
-                    }
-                    try {
-                        JsonNode response = table.list(querySpec -> {
-                            querySpec.sort("ID");
-                            querySpec.fields("title");
-                        })
-                                .get(j).getField("title");
-                        System.out.println(printing);
-                        return response.toPrettyString();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    j++;
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                i++;
-            }
-        }
+class Offset {
+
+    private AtomicInteger offset = new AtomicInteger();
+
+    {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        executorService.scheduleAtFixedRate(() -> offset.incrementAndGet(), 0, 1, TimeUnit.SECONDS);
+    }
+
+    public int get() {
+        return offset.get();
     }
 }
